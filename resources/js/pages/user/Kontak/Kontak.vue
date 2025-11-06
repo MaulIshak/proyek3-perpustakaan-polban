015cbdc70d5fe7c95913c7f55e7007731c7527ab
Faliq Azzaki <faliqazzaki@gmail.com>
2025-11-06 21:25:27 +0700

    Add Embed maps

--- resources/js/pages/user/Kontak/Kontak.vue
+++ resources/js/pages/user/Kontak/Kontak.vue
@@ -98,7 +98,32 @@ const breadcrumb = [
                     </div>
                     <div class="p-6 pt-0">
                         <div class="h-[260px] w-full border rounded-md flex items-center justify-center bg-gray-50">
-                            <p class="text-gray-500">ini adalah map</p>
+                            <div class="embed-map-responsive">
+                                <div class="embed-map-container">
+                                    <iframe
+                                        class="embed-map-frame"
+                                        frameborder="0"
+                                        scrolling="no"
+                                        marginheight="0"
+                                        marginwidth="0"
+                                        src="https://maps.google.com/maps?width=600&height=400&hl=en&q=Politeknik%20Negeri%20Bandung&t=&z=15&ie=UTF8&iwloc=B&output=embed"
+                                    ></iframe
+                                    ><a
+                                        href="https://sprunkiretake.net"
+                                        style="
+                                            font-size: 2px !important;
+                                            color: gray !important;
+                                            position: absolute;
+                                            bottom: 0;
+                                            left: 0;
+                                            z-index: 1;
+                                            max-height: 1px;
+                                            overflow: hidden;
+                                        "
+                                        >sprunki retake</a
+                                    >
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -106,3 +131,28 @@ const breadcrumb = [
         </div>
     </UserAppLayout>
 </template>
+<style>
+.embed-map-responsive {
+    position: relative;
+    text-align: right;
+    width: 100%;
+    height: 0;
+    padding-bottom: 66.66666666666666%;
+}
+.embed-map-container {
+    overflow: hidden;
+    background: none !important;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+.embed-map-frame {
+    width: 100% !important;
+    height: 100% !important;
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+</style>
